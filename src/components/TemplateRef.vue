d925115a5ff734c83759d600db10e1762cba1a90
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-22 17:51:20 +0400

    added template ref

---
+++ src/components/TemplateRef.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+       <input type="text" ref="inputRef">
+       <PostList ref="postListRef"/>
+    </div>
+</template>
+
+<script>
+import PostList from "./PostList.vue";
+    export default {
+        name: 'TemplateRef',
+        mounted(){
+            this.$refs.inputRef.focus();
+            console.log(this.$refs.postListRef); /*by this we can access the DOM node of child */
+        },
+        components: {
+            PostList
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
