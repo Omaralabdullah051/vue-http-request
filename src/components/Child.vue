0122a9145108012b1f5c7f3c83a314898a497f7d
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-22 17:37:01 +0400

    added lifecycle hooks and trace all the phases

---
+++ src/components/Child.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+       Child component
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'childA',
+        beforeCreate(){
+            console.log('Child beforeCreate()');
+        },
+        created(){
+            console.log('Child created()');
+        },
+        beforeMount(){
+            console.log('child beforeMount()');
+        },
+        mounted(){
+           console.log('Child mounted()');
+        },
+        beforeUpdate(){
+            console.log('child beforeUpdate()');
+        },
+        updated(){
+            console.log('child updated()');
+        },
+        beforeUnmount(){
+            console.log('child beforeUnmount()');
+        },
+        unmounted(){
+            console.log('child unmounted()');
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
