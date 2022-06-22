f803ce368905cfbff55d95d4b028fa0414e35d20
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-22 18:02:24 +0400

    used mixins for reusablility

---
+++ src/components/HoverCounter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+       <button @mouseover="incrementCount">Hovered {{count}} times</button>
+    </div>
+</template>
+
+<script>
+import CounterMixin from "../mixins/counter";
+    export default {
+        name: 'HoverCounter',
+        mixins: [CounterMixin]
+    }
+</script>
+
+<style scoped>
+
+</style>
